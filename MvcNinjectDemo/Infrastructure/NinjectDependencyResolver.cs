bb2e7db07ce6361a5f3d834e576fc66ce9584324
masoudparsa <m.parsanejad@gmail.com>
2016-06-14 11:58:48 +0430

    test

---
+++ MvcNinjectDemo/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,49 @@
+﻿using Ninject;
+using Ninject.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcNinjectDemo.Infrastructure
+{
+    public class NinjectDependencyResolver:IDependencyResolver
+    {
+        private IKernel kernel;
+        public IKernel Kernel
+        {
+            get
+            {
+                return this.kernel;
+            }
+        }
+
+        public NinjectDependencyResolver()
+        {
+            this.kernel = new StandardKernel();
+            this.AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        { 
+        }
+
+        public IBindingToSyntax<T> Bind<T>()
+        {
+            return this.kernel.Bind<T>();
+        }
+
+
+    }
+}
---
+++ MvcNinjectDemo/Repository/FakeProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcNinjectDemo.Repository
+{
+    public class FakeProductRepository : IProductRepository
+    {
+    }
+}
